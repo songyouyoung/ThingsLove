521084abc60cfb7583a2a9b75a39c08be33e984d
songyouyoung <dbdud629@naver.com>
2024-06-19 16:25:00 +0900

    write_item js구현중
    이미지 업로드
    > 이미지 미리보기 구현 완료
    > 이미지 제거 구현 완료

--- js/write_item.js
+++ js/write_item.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
 // /////////////////////////////////////////////
-// 사진 클릭 시 이미지 업로드 및 이미지 미리보기
+// 사진 클릭 시 이미지 미리보기
 // /////////////////////////////////////////////
     $(document).on("click", ".w_file_box", function(event) {
         event.stopPropagation()
+        if ($(event.target).hasClass('w_file_close')) { return; }
         $(this).children(".w_file_input").click();
     });
     $(document).on("click", ".w_file_input", function(event) {
@@ -11,120 +12,46 @@ $(document).ready(function(){
     });
     $(".w_file_input").change(getImageFiles);
 
-    let uploadFiles = [];
-    let files = null;
+    let file = null;
     function getImageFiles(e) {
-        files = e.currentTarget.files;
-        console.log("들어옴");
-        console.log(files);
-        const imagePreview = document.querySelector('.w_m_file_box');
+        file = e.currentTarget.files[0];
+        const uploadBox = $(this).nextAll('.w_file_upload');
+        const imageClose = $(this).nextAll('.w_file_close');        
+        const imagePreview = $(this).parent(); // w_file_box
 
-    //     // 파일 갯수 검사
-    //     if ([...files].length + class_cnt < 6) {
-    //         [...files].forEach(file => {
-    //             let chk = true;
-    //             // 파일 타입 검사
-    //             if (!file.type.match("image/.*")) {
-    //                 Swal.fire({
-    //                     icon: "warning",
-    //                     title: "이미지 파일만 업로드가 가능합니다."
-    //                 });
-    //                 return;
-    //             }
-    //             for (let i = 0; i<class_cnt; i++){
-    //                 if (file.name == $(".w_m_file_item").eq(i).children("img").data("file")){
-    //                     Swal.fire({
-    //                         icon: "warning",
-    //                         title: "중복된 이미지 파일입니다. "
-    //                     });
-    //                     return;
-    //                 }
-    //             }
-    //             // 파일 중복 검사
-    //             [...uploadFiles].forEach(upload => {
-    //                 if (upload.name == file.name){
-    //                     Swal.fire({
-    //                         icon: "warning",
-    //                         title: "중복된 이미지 파일입니다. "
-    //                     });
-    //                     chk = false;
-    //                     return false;
-    //                 }
-    //                 if(!chk){return;}
-    //             });
-    //             if(!chk){return;}
+        let chk = true;
+        // 파일 타입 검사
+        if (!file.type.match("image/.*")) {
+            Swal.fire({
+                icon: "warning",
+                title: "이미지 파일만 업로드가 가능합니다."
+            });
+            return;
+        }
 
-    //             uploadFiles.push(file);
-    //             const reader = new FileReader();
-    //             reader.onload = (e) => {
-    //                 const preview = createElement(e, file);
-    //                 imagePreview.appendChild(preview);
-    //             };
-    //             reader.readAsDataURL(file);
-    //             if(uploadFiles.length + class_cnt >= 5){ $(".w_m_file_upload").css({display:"none"}); }
-    //             else{$(".w_m_file_upload").css({display:"flex"});}
-    //         });
-    //     }else{
-    //         Swal.fire({
-    //             icon: "warning",
-    //             title: "이미지는 최대 5개 까지 업로드가 가능합니다."
-    //         });
-    //         return;
-    //     }
-    //     fileControll();
-    // }
-    // function createElement(e, file) {
-    //     const div = document.createElement('div');
-    //     div.setAttribute('class', 'w_m_file_item');
-    //     const img = document.createElement('img');
-    //     img.setAttribute('src', e.target.result);
-    //     img.setAttribute('data-file', file.name);
-    //     const close = document.createElement('div');
-    //     let closeTxt = document.createTextNode("X");
-    //     close.appendChild(closeTxt);
-    //     close.setAttribute('class', "w_m_close");
-    //     div.appendChild(close);
-    //     div.appendChild(img);
-    //     return div;
-    // }
-    // const realUpload = document.querySelector('#w_m_file_input');
-    // const upload = document.querySelector('.w_m_file_upload');
-    // upload.addEventListener('click', () => {
-    //     realUpload.click() });
-    // realUpload.addEventListener('change', getImageFiles);
-
-    // let cancelTxt = "";
-    // $(document).on('click', '.w_m_close', (e)=>{
-    //     let closeImg = e.target.parentElement.getElementsByTagName("img")[0].getAttribute("data-file");
-    //     let upload = [];
-    //     let cancel = document.getElementById("w_cancel");
-    //     let cancelChk = true;
-    //     [...uploadFiles].forEach(file => {
-    //         // file.name != closeImg ? upload.push(file) : cancelTxt += closeImg + "|";
-    //         file.name != closeImg ? upload.push(file) : "";
-    //         cancelChk = false;
-    //     });
-    //     if(cancelChk){ cancelTxt += closeImg + "|"; }
-    //     uploadFiles = upload;
-    //     fileControll();
-    //     cancel.value = cancelTxt;
-    //     e.target.parentElement.remove();
-    //     $(".w_m_file_upload").css({display:"flex"});
-    // });
-
-    // function fileControll(){
-    //     // 파일 누적 관리
-    //     let w_input = document.querySelector('#w_input');
-    //     let dataTransfer = new DataTransfer();
-
-    //     // 파일을 DataTransfer에 추가
-    //     for (let i = 0; i < uploadFiles.length; i++) { dataTransfer.items.add(uploadFiles[i]); }
-
-    //     // DataTransfer에서 FileList-like 객체 생성
-    //     let uploader = dataTransfer.files;
-
-    //     // 파일 입력 요소에 설정
-    //     w_input.value = null;
-    //     w_input.files = uploader;
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            uploadBox.addClass("none");
+            imageClose.removeClass("none");
+            imagePreview.css({ backgroundImage: "url("+ e.target.result +")" });
+        };
+        reader.readAsDataURL(file);
+        console.log("file1 : ", file);
     }
+
+// /////////////////////////////////////////////
+// 사진 삭제
+// /////////////////////////////////////////////
+    let cancelTxt = "";
+    $(document).on('click', '.w_file_close', function(event){
+        event.stopPropagation();
+        const uploadInput = $(this).prevAll('.w_file_input');
+        const uploadBox = $(this).prevAll('.w_file_upload');
+        const imagePreview = $(this).parent(); // w_file_box
+
+        uploadBox.removeClass("none");
+        $(this).addClass("none");
+        imagePreview.css({ backgroundImage: "url('')" });
+        uploadInput.val('');
+    });
 });
