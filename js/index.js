31b157d2c2db7df42f191262a38c221f03902321
songyouyoung <dbdud629@naver.com>
2024-06-21 23:14:53 +0900

    index js 구현중
    전체 체크 구현

--- js/index.js
+++ js/index.js
@@ -5,11 +5,8 @@ $(document).ready(function(){
         $(this).toggleClass("check");
         let index = $(".main").find('.m_check_img').index(this)
         if(index == 0) { // 전체 체크
-            console.log("전체")
-            $(".main").find('.m_check_img').each(function() {
-                if ($(this).index() > 0) {
-                    $(this).toggleClass("check");
-                }
+            $(".main").find('.m_check_img').each(function(index) {
+                if (index > 0) { $(this).toggleClass("check"); }
             });
         }
     });
