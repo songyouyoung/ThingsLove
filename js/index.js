078dbb7dd978405be1fee951b4761c890d12cb9b
songyouyoung <dbdud629@naver.com>
2024-06-22 02:18:12 +0900

    index js 구현중
    체크 시 변수에 true / false 체크

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-    // todo:: 개수 가져와서 index 초기화하기
-    let checks;
+    // todo:: 상품 개수 가져와서 checks 초기화하기
+    let checks = new Array(8).fill(false);
     $(document).on('click', '.m_check_img', function(){
         $(this).toggleClass("check");
         let index = $(".main").find('.m_check_img').index(this)
+        checks[index] = checks[index] ? false : true;
         if(index == 0) { // 전체 체크
             $(".main").find('.m_check_img').each(function(index) {
-                if (index > 0) { $(this).toggleClass("check"); }
+                if (index > 0) { 
+                    $(this).toggleClass("check"); 
+                    checks[index] = checks[index] ? false : true;
+                }
             });
         }
     });
